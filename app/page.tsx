ebd6df86b9cb521687daa13051d461fadecc6d88
heitor <pontojasko>
2026-01-09 23:00:15 -0300

    fixes: responsavidade

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,23 @@ export default function Home() {
   return (
 
     
-    <div className="min-h-svh h-svh">
-         <header className="flex justify-center items-baseline-last h-1/4 w-screen  bg-background-900 shadow-2xl/10 ">
+    <div className="min-h-svh h-svh overflow-hidden">
+         <header className="p-10 overflow-hidden flex justify-center items-center h-1/4 w-screen  bg-background-900 shadow-2xl/10 ">
       
-            <h1 className="text-title text-shadow-drop/100 tracking-tighter ">
+            <h1 className=" text-title text-shadow-drop/100 tracking-tighter ">
               Benius
             </h1>
         </header>
         <div className="flex justify-center items-center  w-screen h-1/2  ">
-      <div className="flex justify-center items-center bg-foreground-900 w-100 h-100 shadow-xl/0">
-              <p className="text-3xl text-shadow-minidrop/80">Aqui dentro teria um textinho</p>
+      <div className="
+      p-10 overflow-hidden flex justify-center items-center bg-foreground-900
+      xl:max-w-[22svw] 
+      md:max-w-[30svw]
+      sm:max-w-[50svw]
+      h-svh w-svw max-h-[40svh] max-w-[70svw] shadow-xl/0
+
+       ">
+              <p className=" text-3xl text-shadow-minidrop/80">Aqui dentro teria um textinho</p>
         </div>
 
         </div>
@@ -21,8 +28,9 @@ export default function Home() {
 
         <footer className="flex justify-center items-baseline-last h-1/4 w-screen bg-background-900">
         
-           <div className="flex justify-center items-center h-1/2 w-screen bg-background-800">
-              <p className="text-2xl">by <a href="https://jasko.dev"className="text-2xl">JASKO ^^</a></p>
+           <div className=" overflow-hidden flex justify-center items-center h-1/2 w-screen bg-background-800">
+              <p className=" text-2xl text-shadow-minidrop/100">by 
+              <a href="https://jasko.dev"className="tracking-tighter transition-all hover:text-[1.6rem]"> JASKO ^^</a></p>
             </div>
 
         </footer>
