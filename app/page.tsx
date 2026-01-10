3c5687c2241bb95f47aab912fcb3a94126841a42
heitor <pontojasko>
2026-01-10 00:02:19 -0300

    gameboy

--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,32 @@ export default function Home() {
 
     
     <div className="min-h-svh h-svh overflow-hidden">
-         <header className="p-10 overflow-hidden flex justify-center items-center h-1/4 w-screen  bg-background-900 shadow-2xl/10 ">
+         <header className="p-10 overflow-hidden flex justify-center items-center h-1/4 w-screen  bg-gameboy-3 shadow-2xl/10 ">
       
-            <h1 className=" text-9xl md:text-title text-shadow-drop/100 tracking-tighter ">
+            <h1 className="text-gameboy-1 text-9xl md:text-title text-shadow-drop/100 tracking-tighter ">
               Benius
             </h1>
         </header>
-        <div className="flex justify-center items-center  w-screen h-1/2  ">
+        <div className="flex justify-center items-center  w-screen h-1/2 bg-gameboy-2  ">
       <div className="
-      p-10 overflow-hidden flex justify-center items-center bg-foreground-900
+      p-10 overflow-hidden flex justify-center items-center bg-gameboy-4
       xl:max-w-[22svw] 
       md:max-w-[30svw]
       sm:max-w-[50svw]
       h-svh w-svw max-h-[40svh] max-w-[70svw] shadow-xl/0
 
        ">
-              <p className=" text-3xl text-shadow-minidrop/80">Aqui dentro teria um textinho</p>
+              <p className="text-gameboy-1 text-3xl text-shadow-minidrop/80">Aqui dentro teria um textinho</p>
         </div>
 
         </div>
 
 
-        <footer className="flex justify-center items-baseline-last h-1/4 w-screen bg-background-900">
+        <footer className="flex justify-center items-baseline-last h-1/4 w-screen bg-gameboy-3">
         
-           <div className=" overflow-hidden flex justify-center items-center h-1/2 w-screen bg-background-800">
-              <p className=" text-2xl text-shadow-minidrop/100">by 
-              <a href="https://jasko.dev"className="tracking-tighter transition-all hover:text-[1.6rem]"> JASKO ^^</a></p>
+           <div className=" overflow-hidden flex justify-center items-center h-1/2 w-screen bg-gameboy-4">
+              <p className="text-gameboy-1 text-2xl text-shadow-minidrop/100">by 
+              <a href=" https://jasko.dev"className="text-gameboy-4tracking-tighter transition-all hover:text-[1.6rem]"> JASKO ^^</a></p>
             </div>
 
         </footer>
