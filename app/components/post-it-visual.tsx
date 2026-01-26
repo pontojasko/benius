b89f4996bc7cb599735cce620daa2892d15338f4
heitor <pontojasko>
2026-01-26 17:56:13 -0300

    fix: adicionar botao novo de submit

--- app/components/post-it-visual.tsx
+++ app/components/post-it-visual.tsx
@@ -19,20 +19,40 @@ export default function PostItVisual(props: PostItProps) {
   
 
 return (
-<div className="
-      flex justify-center items-center overflow-hidden md:p-12 p-7 bg-gameboy-4
+<div className=" 
+      flex justify-center items-center overflow-hidden p-[clamp(1rem,5vw,3rem)] bg-gameboy-4
       xl:max-h-[40svh] md:max-h-[40svh] sm:max-h-[40svh]
       h-svh max-h-[35svh] shadow-drop/100 aspect-square
 
        ">
         {editing ? 
-        <div className="flex justify-center items-center w-full  h-full">
-        <textarea onKeyDown={(e) => { if (e.key === 'Enter' && !e.shiftKey) { e.preventDefault();  } }}
-                  maxLength={90} placeholder={"Qual é a má notícia?"} autoFocus spellCheck={false}
-          className="text-gameboy-1 xl:text-4xl text-2xl text-shadow-minidrop outline-none leading-tight tracking-tight h-full w-full overflow-hidden resize-none">
-        </textarea>
+        <div className="grid grid-rows-[auto_1fr_auto] justify-center items-center w-full  h-full">
+          <div className="flex justify-items items-center">
+
+                <p></p>
+
+          </div>
+                  
+          <div className="flex justify-items items-center h-full">
+
+                      <textarea onKeyDown={(e) => { if (e.key === 'Enter' && !e.shiftKey) { e.preventDefault();  } }}
+                            maxLength={90} placeholder={"Qual é a má notícia?"} autoFocus spellCheck={false}
+                            className="text-gameboy-1 sm:text-4xl text-1xl text-shadow-minidrop outline-none leading-tight tracking-tight w-full h-full overflow-hidden resize-none">
+                      </textarea>
+            </div>
+
+            <div className="flex justify-end w-full">
+                      <button type="submit" className="sm:text-2xl text-1xl outline-none cursor-pointer leading-tight tracking-tight select-none text-shadow-minidrop">&gt; Enter</button>
+              </div>
+
 
         </div>
+
+
+
+
+
+
         :
         <p title="Dois cliques para enviar a sua má notícia!" className="text-gameboy-1 text-[clamp(1.5rem,4vmin,2.5rem)] leading-tight tracking-tight text-shadow-minidrop" onDoubleClick={edit}>{props.texto}</p>}
               
