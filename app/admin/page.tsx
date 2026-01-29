e2e5e7f428045a5fb1dc4cdb8a8f9762f02ceb24
heitor <pontojasko>
2026-01-28 21:44:15 -0300

    feat: adminpage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -12,7 +12,8 @@ export default async function Admin(){
             <div className="flex justify-items-center justify-center items-center py-5">
             <div className="[zoom:0.9] sm:[zoom:0.5] gap-20 justify-center grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 bg-gameboy-2 ">
              
-                  
+                   
+                   
                     {json.map(postit => 
                         <div key={postit.id}>
                             <PostItVisual texto={postit.texto}/>
