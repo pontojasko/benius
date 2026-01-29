41cb640ef6a0fd21c46dc9d3e6d53d610ad8edf3
heitor <pontojasko>
2026-01-29 00:59:53 -0300

    checkpoint

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -17,9 +17,9 @@ export default async function Admin(){
 
 
     return (
-        <main className="w-full h-full">
-            
-            <div className="flex justify-items-center justify-center items-center py-5">
+        <main className="w-full h-full ">
+        
+            <div className=" flex justify-items-center justify-center items-center py-5">
             <div className="[zoom:0.9] sm:[zoom:0.5] gap-20 justify-center grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 bg-gameboy-2 ">
              
                    
